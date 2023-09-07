426dc50a4e5ce53f8ae859dd37e36e70cd5a6b4f
peterdavidfagan <peterdavidfagan@gmail.com>
2023-09-07 16:58:11 +0100

    add hardware interface for arm

--- mujoco_ravens/scene.py
+++ mujoco_ravens/scene.py
@@ -3,24 +3,34 @@
 from typing import Dict, Sequence, Tuple, Union
 
 import numpy as np
-import mujoco
+import jax
+import jax.numpy as jnp
+from jax import random
+
+# arena models
+from dm_robotics.moma.models.arenas import empty
 
 # robot models
 from models.arms import franka_emika
-from dm_robotics.moma import robot
 from dm_robotics.moma.models.end_effectors.robot_hands import robotiq_2f85
-from dm_robotics.moma.models.robots.robot_arms import robot_arm
-from dm_robotics.moma.models.arenas import empty
+from dm_robotics.moma import robot
+
+# props
+from dm_robotics.moma import prop
 
 # robot hardware
 from dm_robotics.moma import sensor
+from dm_robotics.moma.sensors.camera_sensor import CameraImageSensor
 from dm_robotics.moma import effector
+from dm_robotics.moma.effectors import arm_effector
 
 # physics
 from dm_control import composer, mjcf, mujoco
 
 # visualization
+import matplotlib.pyplot as plt
 import PIL.Image
+
 
 def render_scene(physics: mjcf.Physics) -> np.ndarray:
   camera = mujoco.MovableCamera(physics, height=480, width=480)
@@ -53,13 +63,54 @@ def add_robot_and_gripper(arena: composer.Arena) -> Tuple[composer.Entity, compo
     
     return arm, gripper
 
+def _add_block(arena: composer.Arena) -> composer.Entity:
+  block = prop.Block()
+  frame = arena.add_free_entity(block)
+  block.set_freejoint(frame.freejoint)
+  return block
 
 if __name__=="__main__":
-    # build the scene
+    # build the base arena
     arena = build_arena("test")
+
+    # add the robot and gripper to the arena
     arm, gripper = add_robot_and_gripper(arena)
 
-    # visualize the scene
+    # add objects to the arena
+    block = _add_block(arena)
+
+    # visualize the current scene
     physics = mjcf.Physics.from_mjcf_model(arena.mjcf_model)
     image = PIL.Image.fromarray(render_scene(physics))
     image.show()
+
+    # interface for robot control
+    arm_hardware_interface = arm_effector.ArmEffector(
+            arm,
+            action_range_override=None,
+            robot_name = "franka_emika_panda",
+            )
+
+    #gripper_effector = arm_effector.GripperEffector(gripper)
+
+    # Try running the effectors in a loop.
+    num_steps = int(1. / physics.timestep())  # run for a second
+    min_control = arm_hardware_interface.action_spec(physics).minimum
+    max_control = arm_hardware_interface.action_spec(physics).maximum
+    key = random.PRNGKey(0)
+    arm_command = jax.vmap(random.uniform, in_axes=(None, None, None, 0, 0), out_axes=0)(
+            key,
+            (1,),
+            jnp.float32,
+            min_control,
+            max_control,
+            ).squeeze().__array__()
+
+    for _ in range(num_steps):
+      arm_hardware_interface.set_control(physics, arm_command)
+      physics.step()
+
+    # Visualize the new state of things.
+    after = render_scene(physics)
+    after = PIL.Image.fromarray(after)
+    after.show()
