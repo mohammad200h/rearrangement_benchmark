439761319282c2b72d166400e7194d44fe39e235
peterdavidfagan <peterdavidfagan@gmail.com>
2023-10-02 12:25:04 +0100

    non blocking call

--- mujoco_ravens/ros2_start_docker.py
+++ mujoco_ravens/ros2_start_docker.py
@@ -1,6 +1,5 @@
 """A script to test ROS 2 functionality."""
 import os
-import sys
 import subprocess
 
 
@@ -8,14 +7,7 @@ def start_control_server():
     """Start the control server."""
     file_dir = os.path.dirname(os.path.abspath(__file__))
     docker_compose_dir = os.path.join(file_dir, "..", ".docker", "control")
-    success = subprocess.call(["docker", "compose", "-f", "docker-compose-control.yaml", "up"], cwd=docker_compose_dir)
-
-    if success:
-        print("Control server started successfully.")
-        sys.exit(0)
-    else:
-        print("Control server failed to start.")
-        sys.exit(1)
+    subprocess.Popen(["docker", "compose", "-f", "docker-compose-control.yaml", "up"], cwd=docker_compose_dir)
 
 
 # def start_motion_planning_service():
